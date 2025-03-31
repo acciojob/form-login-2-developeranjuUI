945f2e699b5e1adc857bb7c5a858e74433f91bdc
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-31 14:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById('loginForm').addEventListener('submit',()=>{
+	let firstname = document.getElementById('firstname').value;
+	let lastname = document.getElementById('lastname').value;
+	let phonenumber = document.getElementById('phonenumber').value;
+	let email = document.getElementById('email').value;
+
+	let alertMessage = `First Name: ${firstname}\n Last Name: ${lastname}\n Phone Number: ${phonenumber}\n Email ID: ${email}`;
+	alert(alertMessage);
+	
+})
